98ec212803f51e9a07ffc738d0ada0e372ea4e58
issactoast <issactoast@gmail.com>
2026-03-06 12:43:12 +0900

    Rename plugin to wplaybook-all100-custom-block

--- wplaybook-all100-custom-block.php
+++ wplaybook-all100-custom-block.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       WPlaybook Shimmer Button
+ * Plugin Name:       WPlaybook All100 Custom Block
  * Plugin URI:        https://wplaybook.com
  * Description:       Adds a beautiful shimmer effect to buttons with the 'wplaybook-button' class.
  * Version:           1.0.0
@@ -8,24 +8,25 @@
  * Author URI:        https://wplaybook.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wplaybook-shimmer-button
+ * Text Domain:       wplaybook-all100-custom-block
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
 /**
  * Enqueue plugin styles.
  */
-function wplaybook_shimmer_button_enqueue_styles() {
+function wplaybook_all100_custom_block_enqueue_styles()
+{
     wp_enqueue_style(
-        'wplaybook-shimmer-button-style',
-        plugin_dir_url( __FILE__ ) . 'assets/css/wplaybook-shimmer-button.css',
+        'wplaybook-all100-custom-block-style',
+        plugin_dir_url(__FILE__) . 'assets/css/wplaybook-all100-custom-block.css',
         array(),
         '1.0.0',
         'all'
     );
 }
-add_action( 'wp_enqueue_scripts', 'wplaybook_shimmer_button_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'wplaybook_all100_custom_block_enqueue_styles');
